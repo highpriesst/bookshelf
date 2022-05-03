2651f4778c8fd1253e44c684f49cbf4a939c1c59
Ertan Mutlu <ertansofia@gmail.com>
2022-05-03 16:56:55 +0300

    fixing errors

--- src/components/bookCard.component.js
+++ src/components/bookCard.component.js
@@ -1,5 +1,3 @@
-// check google cloud platform or firebase
-
 // import { FaReact } from "react-icons/fa";
 // import { SiTypescript } from "react-icons/si";
 // import { AiOutlineClockCircle } from "react-icons/ai";
@@ -21,12 +19,12 @@ const BookCard = () => {
       <div className="p-5">
         <a href="/">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            The Sandman Vol. 1: Preludes & Nocturnes
           </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          Neil Gaiman's award-winning masterpiece The Sandman is one of the most
+          popular and critically acclaimed graphic novels of all time.
         </p>
         <a
           href="/"
