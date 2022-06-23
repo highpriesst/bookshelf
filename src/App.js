993d2f45f273c60afee6f31c6f1294b1de7a8aab
Ertan Mutlu <ertansofia@gmail.com>
2022-06-23 20:16:03 +0300

    fresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
+
+const url = "";
+
 function App() {
-  return <div className="">bookshelf re-started</div>;
+  const [data, setData] = useState({});
+  const [book, setBook] = useState("");
+
+  async function fetchBooks() {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setData(data.docs[0]);
+    setBook(data.q);
+    console.log(data.docs[0]);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  });
+
+  return <div className="">{book ? <p>{book}</p> : <p>Loading...</p>}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
-
-//This will stay as it is, will add all the components in the
-// app.js file.
-
-//Maybe add Router aka browser router around <App />
-
 ReactDOM.render(
-  <BrowserRouter>
+  <>
     <App />
-  </BrowserRouter>,
+  </>,
   document.getElementById("root")
 );
