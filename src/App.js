a92d6d135d7c2aad9092d8865687778d3e379040
Ertan Mutlu <ertansofia@gmail.com>
2022-06-23 19:47:13 +0300

    fresh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import "./App.css";
-import Navbar from "./components/Nav.component";
-import Footer from "./components/Footer.component";
-import Books from "./components/Books.component";
-
-//Make this App hold state for the boooks.
-//tailwind quick center -> min-h-screen flex flex-col items-center justify-center
-
-//Store the STATE only in here, turn the app component to class component
-// and modify state only in here.
-
 function App() {
-  return (
-    <div className="App flex flex-col transition-colors duration-300 overflow-auto bg-repeat-y dark:bg-gradient-to-r from-slate-600 to-slate-800">
-      <Navbar />
-      <div className=" flex min-h-screen justify-center items-center">
-        <Books />
-      </div>
-      <Footer />
-    </div>
-  );
+  return <div className="">bookshelf re-started</div>;
 }
 
 export default App;
--- src/components/review.componen.js
+++
@@ -1,2 +0,0 @@
-// You add the link below every book card and take it to your 
-//review page, in this review page you share your critics about the books you read before.
