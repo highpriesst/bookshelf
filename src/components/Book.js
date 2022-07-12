250fb6544e538f0a6849baf77443991028ba1912
Ertan Mutlu <ertansofia@gmail.com>
2022-07-12 22:41:02 +0300

    works like a charm

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,13 +3,14 @@ import Search from "./Search";
 import axios from "axios";
 
 function Book() {
-  const [results, setResults] = useState({});
+  const [results, setResults] = useState([]);
   useEffect(() => {
     const searchBestSellers = async () => {
       const res = await axios.get(
         `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_NEWYORK_API_KEY}`
       );
       setResults(res.data.results.books);
+      console.log(res.data.results.books);
     };
     //https://dmitripavlutin.com/react-useeffect-infinite-loop/
     //read this for the useefffect problem that axios not rendering on refresh.
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,22 +3,21 @@ import React from "react";
 function Search({ results }) {
   return (
     // this works when axios load
-    <section>
-      {results?.map((book, i) => {
-        const { author, book_image, contributor, description, title, rank } =
-          book;
+    <section className="grid grid-cols-3 p-5 justify-center items-center">
+      {results.map((book) => {
+        const { author, book_image, description, title, rank } = book;
 
         return (
-          <article
-            key={rank}
-            className="flex justify-center items-center h-screen flex-row">
-            <div>
-              <h2>{title}</h2>
-              <span>{rank}</span>
-              <h3>{author}</h3>
-              <img src={book_image} alt="cover" style={{ height: 200 }} />
+          <article key={rank} className="p-4">
+            <div className="border border-black max-h-80 max-w-80">
+              <p className=" float-left mr-3">{rank}</p>
+              <p className="">{author}</p>
+              <img
+                src={book_image}
+                alt={title}
+                style={{ height: 200, width: 200 }}
+              />
               <p>{description}</p>
-              <span>{contributor}</span>
             </div>
           </article>
         );
